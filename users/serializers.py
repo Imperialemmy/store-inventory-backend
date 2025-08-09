8b6f9d3e5554ecbd555ed045ba9087a9f2acc299
oluwaseun <oluwaseunadeeko124@gmail.com>
2025-08-09 10:05:31 +0100

    feat: created an automated function to assign first user to signup as an admin

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from djoser.serializers import UserSerializer as BaseUserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import CustomUser
+from django.db import transaction
 
 # Used during registration (POST /auth/users/)
 class CustomUserCreateSerializer(BaseUserCreateSerializer):
@@ -10,6 +11,19 @@ class CustomUserCreateSerializer(BaseUserCreateSerializer):
         model = CustomUser
         fields = ('id', 'username','first_name','last_name', 'email', 'password', 'phone_number')
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Avoid race conditions if two people sign up at the same time
+        with transaction.atomic():
+            is_first_user = CustomUser.objects.select_for_update().count() == 0
+            user = super().create(validated_data)  # uses create_user, hashes password
+
+            if is_first_user:
+                user.role = "admin"
+                user.is_staff = True
+                user.is_superuser = True
+                user.save(update_fields=["role", "is_staff", "is_superuser"])
+        return user
 
 # Used when retrieving user info (GET /auth/users/me/)
 class CustomUserSerializer(BaseUserSerializer):
